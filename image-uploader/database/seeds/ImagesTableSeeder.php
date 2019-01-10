3b3e7532e7ae7547761a35d375221169caff5abc
shilin <shilin.dev@gmail.com>
2019-01-11 01:27:31 +0300

    Добавлены роуты,модели, миграции, контроллеры для работы приложения

---
+++ image-uploader/database/seeds/ImagesTableSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+use App\Image;
+use Illuminate\Database\Seeder;
+
+class ImagesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // Let's truncate our existing records to start from scratch.
+        Image::truncate();
+
+        $faker = \Faker\Factory::create();
+
+        // And now, let's create a few articles in our database:
+        for ($i = 0; $i < 50; $i++) {
+            Image::create([
+                'filename' => $faker->sentence
+            ]);
+        }
+    }
+}
--- image-uploader/routes/api.php
+++ image-uploader/routes/api.php
@@ -16,3 +16,8 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('/upload', 'API\UploadController@upload');
+
+Route::get('images/{io}','API\ViewController@getImage');
+
